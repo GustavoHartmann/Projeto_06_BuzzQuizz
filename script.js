9dbd74b39f17bdfe2f9875b1839867284669bc54
Gustavo Hartmann <gustavolerner.hartmann@gmail.com>
2022-08-31 18:09:58 -0300

    Ajuste do layout bota de criar o quizz

--- script.js
+++ script.js
@@ -2,39 +2,39 @@ const paginaQuiz = document.querySelector('.paginaQuiz');
 const conteudo = document.querySelector('.conteudo');
 const statusCode404 = 404;
 
-function abrirQuiz(){
+function abrirQuiz() {
     let promessaQuiz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/5000');
     promessaQuiz.then(sucessoQuiz)
     promessaQuiz.catch(erroQuiz)
 }
 function sucessoQuiz(resposta) {
     console.log(resposta.data)
-    paginaQuiz.innerHTML=
-    `<div class="banner">
+    paginaQuiz.innerHTML =
+        `<div class="banner">
         <img class="bannerImg" src="${resposta.data.image}"></img>
         <h1>${resposta.data.title}</h1>
     </div>`
-    
-    for (let i=0; i<resposta.data.questions.length; i++){
+
+    for (let i = 0; i < resposta.data.questions.length; i++) {
         resposta.data.questions[i].answers.sort(comparador)
 
-        paginaQuiz.innerHTML+=`
+        paginaQuiz.innerHTML += `
         <div class="questaoDiv p${i}">
             <div class="titulo">${resposta.data.questions[i].title}</div>
             <div class=respostasDiv></div>
         </div>`
 
         let respostasDiv = document.querySelector(`.p${i} .respostasDiv`)
-        for (let j=0; j<resposta.data.questions[i].answers.length; j++){
-        
-            respostasDiv.innerHTML+=
-                    `<div class="resposta">
+        for (let j = 0; j < resposta.data.questions[i].answers.length; j++) {
+
+            respostasDiv.innerHTML +=
+                `<div class="resposta">
                         <img class="imgResposta" src="${resposta.data.questions[i].answers[j].image}"></img>
                         <span class="textoResposta">${resposta.data.questions[i].answers[j].text}<span>
                     </div>`}
     }
 }
-function erroQuiz(resposta){
+function erroQuiz(resposta) {
     console.log(resposta)
 }
 //abrirQuiz()
@@ -50,7 +50,7 @@ BuscarQuizzes();
 
 function ListarQuizzes(resposta) {
     const listaQuizzes = resposta.data;
-    const quizzes = document.querySelector(".quizzes");
+    const quizzes = document.querySelector(".todos-quizzes .quizzes");
 
     for (let i = 0; i < listaQuizzes.length; i++) {
         quizzes.innerHTML += `
