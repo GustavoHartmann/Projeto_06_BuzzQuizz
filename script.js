067912506a75698bbabb74de22bb75f787ebd3ec
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-08-31 14:05:07 -0300

    Axios

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("teste")
+const paginaQuiz = document.querySelector('.paginaQuiz')
+
+function abrirQuiz(){
+    let promessaQuiz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/105');
+    promessaQuiz.then(sucessoQuiz)
+    promessaQuiz.catch(erroQuiz)
+}
+function sucessoQuiz(resposta){
+    console.log(resposta.data)
+    paginaQuiz.innerHTML=""
+    paginaQuiz.innerHTML=`<div class="escurece"><img class="bannerImg" src="${resposta.data.image}"></img></div>`
+}
+function erroQuiz(resposta){
+    console.log(resposta)
+}
+abrirQuiz()
